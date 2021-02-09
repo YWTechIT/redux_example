ec721d51f2b1f92a92bf4450359f21d7681fcf77
YWTechIT <abcd8637@naver.com>
2021-02-09 18:51:03 +0900

    add Reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,40 +1,45 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from "react";
+import { CounterReduce, doIncrease, doDecrease, changeDiff } from "./reducer";
 
 const Counter = () => {
+  // 초기 선언 값
+  // const [number, setNumber] = useState(0);
+  // const [diff, setDiff] = useState(1);
+  // const [counter, setCounter] = useState({'number':0, 'diff': 1})
+  // const {number, diff} = counter
 
-    // 초기 선언 값
-    // const [number, setNumber] = useState(0);
-    // const [diff, setDiff] = useState(1);
-    const [counter, setCounter] = useState({'number':0, 'diff': 1})
-    const {number, diff} = counter
-    
-    const changeDiff = (e) => {
-        const diff = parseInt(e.target.value);
-        setCounter((counter) => ({...counter, diff: diff})) ;
-    }
-    const onIncrease = () => {
-        setCounter((counter) => ({...counter, number: number + diff}))
-    }
-    const onDecrease = () => {
-        setCounter((counter) => ({...counter, number: number - diff}))
-    }
-    
-    return(
-        <>
-            <h1>{number}</h1>
-            <h1>{diff}</h1>
+  const [{ number, diff }, dispatch] = useReducer(CounterReduce, {
+    number: 0,
+    diff: 1,
+  });
 
-            <input
-            value={diff}
-            placeholder='숫자를 입력하세요'
-            onChange={changeDiff}
-            ></input>
+  const onIncrease = () => {
+    dispatch(doIncrease());
+  };
+  const onDecrease = () => {
+    dispatch({'type': 'decrease'});
+  };
 
-            <button onClick={onIncrease}>+</button>
-            <button onClick={onDecrease}>-</button>
+  const onChange = (e) => {
+    const value = parseInt(e.target.value);
+    dispatch({ type: "diff", payload: value });
+  };
 
-        </>
-    )
-}
+  return (
+    <>
+      <h1>{number}</h1>
+      <h1>{diff}</h1>
+
+      <input
+        value={diff}
+        placeholder="숫자를 입력하세요"
+        onChange={onChange}
+      ></input>
+
+      <button onClick={onIncrease}>+</button>
+      <button onClick={onDecrease}>-</button>
+    </>
+  );
+};
 
 export default Counter;
