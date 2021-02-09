d8a66b3c42afaab82205689611c31f26712a323f
YWTechIT <abcd8637@naver.com>
2021-02-09 19:57:00 +0900

    add CounterReducer
    reducer와 Action create를 선언한 코드이다.
    action type은 unique한 특성을 갖고있어야 나중에 useDispatch()할때 꼬이지 않는다.

---
+++ src/modules/CounterReducer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// Action Increase 객체는 {'type': 'increase'}
+// Action Decrease 객체는 {'type': 'decrease'}
+// Action diff 객체는 {'type': 'diff', 'payload': 3}
+
+const initialState = ({'number': 0, 'diff': 1});
+
+const CounterReducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'increase':
+            return {...state, number: state.number + state.diff};
+        case 'decrease':
+            return {...state, number: state.number - state.diff};
+        case 'diff':
+            return {...state, diff: action.payload};
+        default:
+            return state;   
+    }   
+}
+
+// action 객체가 'type': 'increase'
+// action 객체가 'type': 'decrease'
+// action 객체가 'type': 'diff'
+const doIncrease = () => ({'type': 'increase'});
+const doDecrease = () => ({'type': 'decrease'});
+const changeDiff = (payload) => ({'type': 'diff'});
+
+export {CounterReducer, doIncrease, doDecrease, changeDiff};
+
